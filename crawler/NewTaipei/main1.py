30ef9ab9bcb8bfa0374cf06e14151e7606971a39
cherites <cherites0610@gmail.com>
2024-10-26 00:01:54 +0800

    feat:爬蟲基本框架完成

--- crawler/NewTaipei/main1.py
+++ crawler/NewTaipei/main1.py
@@ -1,12 +1,39 @@
 import sys
+import requests
+from bs4 import BeautifulSoup
+import json
+
+def scrape_data(city, url):
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, "html.parser").select(".con")[0].find_all("div")
+    res.close()
+
+    results = []  # 用于存储所有的 URL 和 title
+
+    for div_list in soup:
+        a = div_list.find_all("span")[1].find("a")
+        detail_url = "https://www.sw.ntpc.gov.tw/" + a["href"]
+        res = requests.get(detail_url)
+        detail_soup = BeautifulSoup(res.text, "html.parser").select(".con")
+        res.close()
+
+        for div_list_2 in detail_soup:
+            a_list = div_list_2.find_all("a")
+            for a in a_list:
+                url = "https://www.sw.ntpc.gov.tw/" + a["href"]
+                title = a["title"]
+                results.append({"url": url, "title": title})  # 将 URL 和 title 存储为字典并添加到列表中
+
+    return results  # 返回包含所有结果的列表
 
 def main():
     # 接收從主程式傳入的參數
     city = sys.argv[1]
     url = sys.argv[2]
-    
-    # 打印 city 和 url
-    print(f"City: {city}, URL: {url}")
+
+    data = scrape_data(city, url)  # 调用 scrape_data 函数获取数据
+    json_data = json.dumps(data, ensure_ascii=False)  # 将数据转换为 JSON 格式
+    print(json_data)  # 输出 JSON 数据
 
 if __name__ == "__main__":
     main()
--- crawler/main.py
+++ crawler/main.py
@@ -1,9 +1,13 @@
+import json
+import os
 import pandas as pd
 import subprocess
 
 # 載入 Excel 檔案
 file_path = 'list.xlsx'
 df = pd.read_excel(file_path)
+
+results = []
 
 # 迭代每一列，呼叫對應的爬蟲腳本並傳遞 city 和 url
 for index, row in df.iterrows():
@@ -12,4 +16,23 @@ for index, row in df.iterrows():
     script_path = str(row['name'])  # 這裡的 name 是腳本路徑
     
     # 執行對應的爬蟲腳本，並將 city 和 url 作為參數傳遞
-    subprocess.run(['python', script_path, city, url])
+    result = subprocess.run(['python', script_path, city, url],capture_output=True, text=True)
+    
+    output = result.stdout.strip()
+    results.append({"script": script_path, "output": output})  # 儲存結果
+
+    print(f"Received from {script_path}: {output}")
+
+    # 如果子腳本失敗（返回非0狀態碼），則記錄錯誤並繼續執行
+    if result.returncode != 0:
+        print(f"Error occurred in {script_path}. Continuing execution.")
+        print(f"Error message: {result.stderr.strip()}")  # 打印錯誤信息
+
+# 定義 JSON 檔案的路徑
+json_file_path = os.path.join(os.path.dirname(__file__), 'data.json')
+
+# 將結果儲存為 JSON 格式
+with open(json_file_path, 'w', encoding='utf-8') as json_file:
+    json.dump(results, json_file, ensure_ascii=False, indent=4)  # 保存為 JSON 文件
+
+print(f"Results saved to {json_file_path}")
