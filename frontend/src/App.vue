832b6bb2fd61309b3092f6cdb5095d2d84c4e050
Luin1223 <154805879+Luin1223@users.noreply.github.com>
2024-10-26 15:13:09 +0800

    fit : 頁面導航欄錯誤修正

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,7 +10,7 @@ const route = useRoute(); // 獲取當前路由
   <!-- 渲染當前頁面 -->
   <RouterView />
   <router-view></router-view>
-  <BottomNav v-if="route.name !== 'LoginPage' && route.name !== 'RegisterPage'" />
+  <BottomNav v-if="!['LoginPage', 'RegisterPage','ForgetPasswordPage','ResetPasswordPage','CreateProfile','VerifyPage'].includes(route.name)" />
 </template>
 
 <style scoped>
