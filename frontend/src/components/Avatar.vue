1bf364d6abfb03c5c8e7a25b1ca9b7e25588dc41
ZY324 <emma93324@gamil.com>
2024-10-26 17:42:51 +0800

    Feat: 用戶頁面建立

---
+++ frontend/src/components/Avatar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col items-center mt-5 text-center">
+    <div class="w-24 h-24 flex justify-center items-center">
+      <img src="/logo.png" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+/* 可選樣式 */
+</style>
