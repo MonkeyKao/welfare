91e77a91abe907d3f5586ff96d01af7bcc696c55
ZY324 <emma93324@gamil.com>
2024-10-26 15:13:58 +0800

    Fix: App.vue 修正routerview

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
-import { RouterView } from 'vue-router'; // 引入 RouterView 用來動態渲染路由對應的頁面
-import BottomNav from '@/components/BottomNav.vue'; // 引入底部導航欄
-import { useRoute } from 'vue-router'; // 引入 useRoute
+import BottomNav from "@/components/BottomNav.vue"; // 引入底部導航欄
+import { RouterView, useRoute } from "vue-router"; // 引入 RouterView 用來動態渲染路由對應的頁面
 
 const route = useRoute(); // 獲取當前路由
 </script>
@@ -9,7 +8,6 @@ const route = useRoute(); // 獲取當前路由
 <template>
   <!-- 渲染當前頁面 -->
   <RouterView />
-  <router-view></router-view>
   <BottomNav />
 </template>
 
