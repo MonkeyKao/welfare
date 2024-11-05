f3b98419ae397d19c6010876b1b3ba6f89a10c41
ZY324 <emma93324@gamil.com>
2024-11-05 15:26:27 +0800

    Fix: 登入頁面修正，響應式還在努力中

--- frontend/src/views/LoginPage.vue
+++ frontend/src/views/LoginPage.vue
@@ -1,123 +1,120 @@
 <template>
-  <!-- 固定定位的 header，確保圖片維持在最上方 -->
-  <header>
-    <img v-if="!doingPw" src="../images/login.jpg" />
-    <img v-else src="../images/password.jpg" />
-  </header>
+  <div class="relative h-screen flex flex-col overflow-hidden">
+    <header>
+      <img v-if="!doingPw" src="../images/login.jpg" class="w-full h-auto" />
+      <img v-else src="../images/password.jpg" class="w-full h-auto" />
+    </header>
 
-  <body>
-    <!-- Flexbox 居中表单 -->
-    <form class="flex flex-col w-full h-full px-10 gap-5">
-      <!-- 限制最大宽度 -->
-      <label class="text-4xl font-bold my-3">Login</label>
-      <div
-        class="flex items-center border-2 rounded-md px-3 my-3 ml-10 mr-10 shadow-md"
-      >
-        <PhUser :size="32" color="#4d4d4d" />
-        <input
-          v-model="user.account"
-          class="flex-1 p-3 focus:outline-none"
-          type="text"
-          placeholder="帳號"
-        />
-      </div>
+    <!-- Flexbox 居中表單 -->
+    <div
+      class="flex flex-col w-full md:max-w-md px-10 mt-5 sm:mt-1 flex-grow h-auto"
+    >
+      <form class="flex flex-col gap-5">
+        <!-- 標題 -->
+        <label class="flex text-4xl sm:text-3xl font-bold justify-center"
+          >Login</label
+        >
 
-      <!-- 密碼輸入框 -->
-      <div
-        class="flex items-center border-2 rounded-md px-3 ml-10 my-3 mr-10 shadow-md"
-      >
-        <PhLockKey :size="32" color="#4d4d4d" />
-        <input
-          v-model="user.password"
-          @focus="doingPw = true"
-          @blur="doingPw = false"
-          class="flex-1 p-3 focus:outline-none"
-          :type="showPassword ? 'text' : 'password'"
-          placeholder="密碼"
-        />
-        <PhEyeClosed
-          v-if="!showPassword"
-          @click="togglePassword"
-          :size="32"
-          color="#4d4d4d"
-        />
-        <PhEye v-else @click="togglePassword" :size="32" />
-      </div>
+        <!-- 帳號輸入框 -->
+        <div class="flex w-full border-2 rounded-md shadow-md p-3 items-center">
+          <PhUser :size="32" color="#4d4d4d" class="flex-shrink-0" />
+          <input
+            v-model="user.account"
+            class="mx-2 min-w-0 text-base max-w-xs md:max-w-md lg:max-w-lg sm:text-sm lg:text-lg"
+            type="text"
+            placeholder="帳號"
+          />
+        </div>
 
-      <div class="mt-5 mx-10 flex items-center">
-        <PhSquare v-if="!check" @click="checklogin" :size="20" />
-        <PhCheckSquare v-else @click="checklogin" :size="20" />
-        <span class="ml-2">保持登入</span>
-      </div>
+        <!-- 密碼輸入框 -->
+        <div class="flex w-full border-2 rounded-md shadow-md p-3 items-center">
+          <PhLockKey :size="32" color="#4d4d4d" class="flex-shrink-0" />
+          <input
+            v-model="user.password"
+            @focus="doingPw = true"
+            @blur="doingPw = false"
+            class="mx-2 min-w-0 text-base"
+            :type="showPassword ? 'text' : 'password'"
+            placeholder="密碼"
+          />
+          <PhEyeClosed
+            v-if="!showPassword"
+            @click="togglePassword"
+            :size="32"
+            color="#4d4d4d"
+            class="flex-shrink-0"
+          />
+          <PhEye
+            v-else
+            @click="togglePassword"
+            :size="32"
+            class="flex-shrink-0"
+          />
+        </div>
 
-      <div class="mt-5 mx-10">
+        <!-- 按鈕 -->
         <button
           @click="loginHandler(user)"
-          class="w-full bg-[#90c700] text-white font-bold text-2xl py-3 rounded shadow-md"
+          class="w-full bg-[#90c700] text-white font-bold text-2xl py-3 sm:py-2 rounded shadow-md"
           type="button"
         >
           登入
         </button>
-      </div>
 
-      <div class="mt-5">
         <router-link
           to="/forgot-password"
-          class="text-[#90c700] underline hover:text-[#7ab600]"
+          class="flex text-lg sm:text-base text-[#92c700] justify-center"
         >
           忘記密碼？
         </router-link>
-      </div>
+      </form>
 
-      <div class="mt-20">
-        <div class="flex flex-col items-center">
-          <label>其他登入方式</label>
-          <div class="flex justify-center space-x-4 mt-4">
-            <button
-              @click="loginWithGoogle"
-              class="w-12 h-12 rounded-full flex justify-center items-center shadow bg-[#92c700]"
+      <!-- 底部連結區域 -->
+      <div class="mt-auto pb-5">
+        <div class="flex flex-col justify-center">
+          <div>
+            <label class="flex text-sm sm:text-base justify-center"
+              >其他登入方式</label
             >
-              <PhGoogleLogo :size="32" color="#fff" />
-            </button>
-            <button
-              @click="loginWithFacebook"
-              class="w-12 h-12 rounded-full flex justify-center items-center shadow bg-[#92c700]"
-            >
-              <PhFacebookLogo :size="32" color="#fff" />
-            </button>
+            <div class="flex justify-center space-x-4 mt-4">
+              <button
+                class="w-12 h-12 sm:w-10 sm:h-10 rounded-full flex justify-center items-center shadow bg-[#92c700]"
+              >
+                <PhGoogleLogo :size="24" color="#fff" />
+              </button>
+              <button
+                class="w-12 h-12 sm:w-10 sm:h-10 rounded-full flex justify-center items-center shadow bg-[#92c700]"
+              >
+                <PhFacebookLogo :size="24" color="#fff" />
+              </button>
+            </div>
           </div>
-        </div>
-
-        <div class="flex items-center mt-5">
-          <div class="flex-1 border-t"></div>
-          <span class="mx-3">or</span>
-          <div class="flex-1 border-t"></div>
-        </div>
-
-        <div class="mb-5">
+          <div class="flex items-center">
+            <div class="flex-1 border-t"></div>
+            <span class="mx-3">or</span>
+            <div class="flex-1 border-t"></div>
+          </div>
           <router-link
             to="/register"
-            class="text-[#90c700] underline hover:text-[#7ab600]"
+            class="flex text-lg sm:text-base text-[#92c700] justify-center"
           >
             創建帳號
           </router-link>
         </div>
       </div>
-    </form>
-  </body>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import request from "@/axios";
 import router from "@/router";
 import {
-  PhCheckSquare,
   PhEye,
   PhEyeClosed,
   PhFacebookLogo,
   PhGoogleLogo,
   PhLockKey,
-  PhSquare,
   PhUser,
 } from "@phosphor-icons/vue";
 import { ref } from "vue";
