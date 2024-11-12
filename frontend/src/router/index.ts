c9e3b12d639cfcc5e3d495a7c6bc5caa580e44f8
Luin1223 <154805879+Luin1223@users.noreply.github.com>
2024-11-12 14:32:08 +0800

    fix:修正衝突

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -14,14 +14,13 @@ import PersonalDataPage from "@/views/User/PersonalDataPage.vue";
 import QAPage from "@/views/User/QAPage.vue";
 import SettingsPage from "@/views/User/SettingsPage.vue";
 import UserPage from "@/views/UserPage.vue";
+import NotifyPage from "@/views/NotifyPage.vue";
 import {
   createRouter,
   createWebHistory,
   RouterView,
   type RouteRecordRaw,
 } from "vue-router";
-import NotifyPage from "@/views/NotifyPage.vue";
-import { createRouter, createWebHistory, RouterView, type RouteRecordRaw } from "vue-router";
 
 const routes: Array<RouteRecordRaw> = [
   {
