cffead54ef2d9da86fe44eabf85c26c01232f59a
feather1217 <609sophie@gmail.com>
2024-11-14 03:24:19 +0800

    fix : 修改阿哞的icon

--- frontend/src/components/BottomNav.vue
+++ frontend/src/components/BottomNav.vue
@@ -3,13 +3,17 @@
     <ul class="flex justify-around items-center">
       <li>
         <router-link to="/ai" class="flex flex-col items-center text-center">
-          <PhRobot
-            :size="32"
-            :weight="isActive('/ai') ? 'fill' : 'regular'"
-            :class="{ 'text-green-500': isActive('/ai') }"
-            :style="{ color: isActive('/ai') ? '#92C700' : 'black' }"
+          <img
+            v-if="isActive('/ai')"
+            src="../images/amuoclick.png"
+            class="h-7"
           />
-          <span v-if="isActive('/ai')" class="text-gray-800">阿哞</span>
+          <img
+            v-else
+            src="../images/amuo.png"
+           class="h-7"
+          />
+          <span v-if="isActive('/ai')" class="text-gray-800 text-H4">阿哞</span>
         </router-link>
       </li>
       <li>
@@ -20,7 +24,7 @@
             :class="{ 'text-green-500': isActive('/home') }"
             :style="{ color: isActive('/home') ? '#92C700' : 'black' }"
           />
-          <span v-if="isActive('/home')" class="text-gray-800">首頁</span>
+          <span v-if="isActive('/home')" class="text-gray-800 text-H4">首頁</span>
         </router-link>
       </li>
       <li>
@@ -31,8 +35,8 @@
             :class="{ 'text-green-500': isActive('/user') }"
             :style="{ color: isActive('/user') ? '#92C700' : 'black' }"
           />
-          <span v-if="isActive('/user')" class="text-gray-800">用戶</span>
-          <span v-if="isActive('/settings')" class="text-gray-800">用戶</span>
+          <span v-if="isActive('/user')" class="text-gray-800 text-H4">用戶</span>
+          <span v-if="isActive('/settings')" class="text-gray-800 text-H4">用戶</span>
         </router-link>
       </li>
     </ul>
