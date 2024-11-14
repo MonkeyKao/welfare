5f12075734c811f836ac57855d2f0ba0465f410b
feather1217 <609sophie@gmail.com>
2024-11-15 02:10:11 +0800

    feat : 註冊ui完畢

--- frontend/src/views/Login/RegisterPage.vue
+++ frontend/src/views/Login/RegisterPage.vue
@@ -1,53 +1,62 @@
 <template>
-  <div class="flex flex-col h-screen w-full">
+  <div class="flex flex-col h-screen w-full justify-between">
     <div class="flex-shrink-0">
       <!-- 固定高度的 header -->
-      <img v-if="!doingPw" src="../../images/login.jpg" />
-      <img v-else src="../../images/password.jpg" />
+      <img v-show="!doingPw" src="../../images/login.jpg" />
+      <img v-show="doingPw" src="../../images/password.jpg" />
     </div>
 
-    <div class="flex flex-col flex-grow">
+    <div class="flex-grow">
       <!-- Flexbox 居中表单 -->
-      <form class="flex flex-col gap-5 px-10 mt-5">
+      <form class="flex flex-col px-10 mt-3 space-y-5">
         <!-- 限制最大宽度 -->
-        <label class="flex text-4xl sm:text-3xl font-bold justify-center">Register</label>
-        <div class="flex border-2 rounded-md shadow-md p-3 items-center">
+        <label class="flex text-H1 font-bold justify-center">Register</label>
+
+        <div class="flex border-2 rounded-md shadow-md y-2 p-1 px-3 items-center">
           <PhUser :size="32" color="#4d4d4d" class="flex-shrink-0" />
           <input v-model="user.account" class="mx-2 min-w-0 text-base" type="text" placeholder="帳號" />
         </div>
         <!-- 密碼輸入框 -->
-        <div class="flex border-2 rounded-md shadow-md p-3 items-center">
-          <PhLockKey :size="32" color="#4d4d4d" class="flex-shrink-0" />
-          <input v-model="user.password" @focus="doingPw = true" @blur="doingPw = false" class="mx-2 min-w-0 text-base"
-            :type="showPassword ? 'text' : 'password'" placeholder="密碼" />
-          <PhEyeClosed v-if="!showPassword" @click="togglePassword" :size="32" color="#4d4d4d" class="flex-shrink-0" />
-          <PhEye v-else @click="togglePassword" :size="32" class="flex-shrink-0" />
-        </div>
-        <div class="flex w-full border-2 rounded-md shadow-md p-3 items-center">
+        <div class="flex border-2 rounded-md shadow-md p-1 px-3 justify-between">
+            <div class="flex">
+              <PhLockKey :size="32" color="#4d4d4d" class="flex-shrink-0" />
+              <input
+                v-model="user.password"
+                @focus="doingPw = true"
+                @blur="doingPw = false"
+                :type="showPassword ? 'text' : 'password'"
+                placeholder="密碼"
+                class="border-none resize outline-none p-1 w-full px-2"
+              >
+            </div>
+            <PhEyeClosed v-if="!showPassword" @click="togglePassword" :size="32" color="#4d4d4d" class="flex-shrink-0" />
+            <PhEye v-else @click="togglePassword" :size="32" class="flex-shrink-0" />
+          </div>     
+
+        <div class="flex border-2 rounded-md shadow-md y-2 p-1 px-3 items-center">
           <PhEnvelopeSimple :size="32" color="#4d4d4d" class="flex-shrink-0" />
           <input v-model="user.email" class="mx-2 min-w-0 text-base" type="email" placeholder="信箱" />
         </div>
 
-        <div class="flex items-center">
+        <div class="flex items-center px-2">
           <PhSquare v-if="!check" @click="checklogin" :size="20" />
           <PhCheckSquare v-else @click="checklogin" :size="20" />
           <span class="ml-2 text-lg">保持登入</span>
         </div>
 
-        <div>
-
-            <button @click="registerHandler(user)"
-              class="w-full bg-[#90c700] text-white font-bold text-2xl py-3 sm:py-2 rounded shadow-md" type="button">
-              創建
-            </button>
+        <div  class="flex flex-col">
+          <button @click="registerHandler(user)" class="bg-[#90c700] text-white text-H3 py-2 rounded shadow-md" type="button">創建</button>
         </div>
       </form>
     </div>
+
     <div class="flex-shrink-0">
-      <div class="flex items-center mt-44">
-        <div class="flex-1 border-t"></div>
-        <span class="mx-3">or</span>
-        <div class="flex-1 border-t"></div>
+      <div class="flex flex-col justify-center">
+        <div class="flex items-center">
+          <div class="flex-1 border-t"></div>
+          <span class="mx-3">or</span>
+          <div class="flex-1 border-t"></div>
+        </div>
       </div>
       <div>
         <router-link to="/account/login" class="flex text-lg text-[#92c700] justify-center">
