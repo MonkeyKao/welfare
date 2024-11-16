4c385c04206c9e3908e3ff49ae15cc0a0cccd05d
ZY324 <emma93324@gamil.com>
2024-11-16 16:23:54 +0800

    feat: 老人首頁切換頁面，發現每個頁面的返回鍵要新增回老人首頁的路由，響應式沒完成

--- frontend/src/views/OlderHomePage.vue
+++ frontend/src/views/OlderHomePage.vue
@@ -1,29 +1,17 @@
 <template>
-  <div class="flex flex-col bg-[#92c700] h-screen p-4 gap-10">
+  <div class="flex flex-col bg-[#92c700] h-screen p-4 gap-5">
     <!-- 快速福利查詢區塊 -->
-    <div class="text-center">
+    <div class="text-center mt-2">
       <h2 class="text-H1 font-bold">快速福利查詢</h2>
-      <div class="border-t-2 border-[#3e3e3e] my-2"></div>
+      <div class="border-t-2 my-2 border-black"></div>
       <div class="flex flex-wrap justify-between gap-2">
         <button
-          class="text-H2 bg-white font-bold py-2 rounded shadow-md w-[48%]"
+          v-for="(item, index) in welfareItems"
+          :key="index"
+          class="text-H2 bg-white font-bold py-2 rounded-lg shadow-md w-[48%]"
+          @click="showMessage(item)"
         >
-          醫療補助
-        </button>
-        <button
-          class="text-H2 bg-white font-bold py-2 rounded shadow-md w-[48%]"
-        >
-          老人年金
-        </button>
-        <button
-          class="text-H2 bg-white font-bold py-2 rounded shadow-md w-[48%]"
-        >
-          長照補助
-        </button>
-        <button
-          class="text-H2 bg-white font-bold py-2 rounded shadow-md w-[48%]"
-        >
-          老人生活津貼
+          {{ item }}
         </button>
       </div>
     </div>
@@ -32,55 +20,16 @@
 
     <div class="text-center">
       <h2 class="text-H1 font-bold">功能選項</h2>
-      <div class="border-t-2 border-[#3e3e3e] my-2"></div>
-      <div class="flex flex-wrap justify-between gap-2">
+      <div class="border-t-2 my-2 border-black"></div>
+      <div class="flex flex-wrap justify-between gap-3">
         <button
-          class="flex flex-col items-center bg-white p-4 rounded shadow-md w-[48%]"
+          v-for="(item, index) in menuItems"
+          :key="index"
+          class="flex flex-col items-center bg-white p-2 rounded-md shadow-xl w-[48%]"
+          @click="router.push(item.path)"
         >
-          <PhHeart size="50" color="#3e3e3e" />
-          <span class="text-H1 font-bold">收藏</span>
-        </button>
-        <button
-          class="flex flex-col items-center bg-white p-4 rounded shadow-md w-[48%]"
-        >
-          <PhBell size="50" color="#3e3e3e" />
-          <span class="text-H1 font-bold">通知</span>
-        </button>
-        <button
-          class="flex flex-col items-center bg-white p-4 rounded shadow-md w-[48%]"
-        >
-          <PhMagnifyingGlass size="50" color="#3e3e3e" />
-          <span class="text-H1 font-bold">福利查詢</span>
-        </button>
-        <button
-          class="flex flex-col items-center bg-white p-4 rounded shadow-md w-[48%]"
-        >
-          <PhRobot size="50" color="#3e3e3e" />
-          <span class="text-H1 font-bold">語音查詢</span>
-        </button>
-        <button
-          class="flex flex-col items-center bg-white p-4 rounded shadow-md w-[48%]"
-        >
-          <PhUser size="50" color="#3e3e3e" />
-          <span class="text-H1 font-bold">個人資料</span>
-        </button>
-        <button
-          class="flex flex-col items-center bg-white p-4 rounded shadow-md w-[48%]"
-        >
-          <PhUsersThree size="55" color="#3e3e3e" />
-          <span class="text-H1 font-bold">家庭</span>
-        </button>
-        <button
-          class="flex flex-col items-center bg-white p-4 rounded shadow-md w-[48%]"
-        >
-          <PhQuestion size="50" color="#3e3e3e" />
-          <span class="text-H1 font-bold">常見問題</span>
-        </button>
-        <button
-          class="flex flex-col items-center bg-white p-4 rounded shadow-md w-[48%]"
-        >
-          <PhGear size="50" color="#3e3e3e" />
-          <span class="text-H1 font-bold">設定</span>
+          <component :is="item.icon" size="60" color="#6E6144" />
+          <label class="text-H1 font-bold">{{ item.label }}</label>
         </button>
       </div>
     </div>
@@ -88,6 +37,7 @@
 </template>
 
 <script setup lang="ts">
+import router from "@/router";
 import {
   PhBell,
   PhGear,
@@ -98,4 +48,18 @@ import {
   PhUser,
   PhUsersThree,
 } from "@phosphor-icons/vue";
+const welfareItems = ["醫療補助", "老人年金", "長照補助", "老人生活津貼"];
+const menuItems = [
+  { path: "/favorites", label: "收藏", icon: PhHeart },
+  { path: "/notifications", label: "通知", icon: PhBell },
+  { path: "/older-home", label: "福利查詢", icon: PhMagnifyingGlass },
+  { path: "/ai", label: "機器查詢", icon: PhRobot },
+  { path: "/user/personal-data", label: "個人資料", icon: PhUser },
+  { path: "/user/family", label: "家庭", icon: PhUsersThree },
+  { path: "/user/qa", label: "常見問題", icon: PhQuestion },
+  { path: "/user/settings", label: "設定", icon: PhGear },
+];
+const showMessage = (item: string) => {
+  console.log(`已點擊: ${item}`);
+};
 </script>
