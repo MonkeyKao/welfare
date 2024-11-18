d550e53b7a341149c3173899aca723862751a87a
feather1217 <609sophie@gmail.com>
2024-11-18 22:47:07 +0800

    fix : 老人主介面修改完畢

--- frontend/src/views/OlderHomePage.vue
+++ frontend/src/views/OlderHomePage.vue
@@ -1,39 +1,39 @@
 <template>
-  <div class="flex flex-col bg-[#92c700] h-screen p-4 gap-5">
-    <!-- 快速福利查詢區塊 -->
-    <div class="text-center mt-2">
-      <h2 class="text-H1 font-bold">快速福利查詢</h2>
-      <div class="border-t-2 my-2 border-black"></div>
-      <div class="flex flex-wrap justify-between gap-2">
-        <button
-          v-for="(item, index) in welfareItems"
-          :key="index"
-          class="text-H2 bg-white font-bold py-2 rounded-lg shadow-md w-[48%]"
-          @click="showMessage(item)"
-        >
-          {{ item }}
-        </button>
-      </div>
-    </div>
-
-    <!-- 功能選項區塊 -->
-
-    <div class="text-center">
-      <h2 class="text-H1 font-bold">功能選項</h2>
-      <div class="border-t-2 my-2 border-black"></div>
-      <div class="flex flex-wrap justify-between gap-3">
-        <button
-          v-for="(item, index) in menuItems"
-          :key="index"
-          class="flex flex-col items-center bg-white p-2 rounded-md shadow-xl w-[48%]"
-          @click="router.push(item.path)"
-        >
-          <component :is="item.icon" size="60" color="#6E6144" />
-          <label class="text-H1 font-bold">{{ item.label }}</label>
-        </button>
-      </div>
+<div class="h-screen bg-[#92c700] flex flex-col space-y-2">
+  <!-- 快速福利查詢區塊 -->
+  <div class="flex-1 basis-1/4 flex flex-col justify-center space-y-2">
+    <h2 class="text-center text-H1 font-bold">快速福利查詢</h2>
+    <div class="border-t-2 my-2 border-black"></div>
+    <div class="grid grid-cols-2 px-5 gap-4 ">
+      <button
+        v-for="(item, index) in welfareItems"
+        :key="index"
+        class="text-H2 bg-white font-bold rounded-lg shadow-md mt-2"
+        @click="showMessage(item)"
+      >
+        {{ item }}
+      </button>
     </div>
   </div>
+
+  <!-- 功能選項區塊 -->
+  <div class="flex-3 basis-3/4 flex flex-col">
+    <h2 class="text-H1 font-bold text-center">功能選項</h2>
+    <div class="border-t-2 my-2 border-black"></div>
+    <div class="grid grid-cols-2 gap-4 p-4 flex-grow">
+      <button
+        v-for="(item, index) in menuItems"
+        :key="index"
+        class="bg-white rounded-lg shadow-md flex flex-col items-center justify-center"
+        @click="router.push(item.path)"
+      >
+        <component :is="item.icon" size="40" color="#6E6144" />
+        <label class="text-H2 font-bold">{{ item.label }}</label>
+      </button>
+    </div>
+  </div>
+</div>
+
 </template>
 
 <script setup lang="ts">
