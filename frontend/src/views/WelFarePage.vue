63fca2df54e92871ccebbd71009889a511dc9d19
feather1217 <609sophie@gmail.com>
2024-12-10 01:37:17 +0800

    fix : 福利詳細資料頁面ui微調

--- frontend/src/views/WelFarePage.vue
+++ frontend/src/views/WelFarePage.vue
@@ -9,23 +9,25 @@
     <div class="flex flex-col p-4">
         <span class=" text-H1 font-bold">{{ welfare.title }}</span>
         <span class=" text-H3 font-bold">{{ welfare.city }}</span>
-        <span class="text-H3">發佈日期:{{ welfare.date ? welfare.date : "無法獲取日期" }}</span>
+        <span class="text-H3">發佈日期 : {{ welfare.date ? welfare.date : "無法獲取日期" }}</span>
         <div class="flex flex-col my-2 space-y-2">
             <span class=" font-bold">申請條件:</span>
-            <span>1.</span>
-            <span>2.</span>
-            <span>3.</span>
+            <div v-for="(item, index) in apply" :key="index" class="grid grid-cols-6 items-center gap-4 py-2 mx-3 border-b border-gray-300 last:border-0">
+                <div class="grid text-center ">{{ item.id }}</div>
+                <div class=" col-span-3">{{ item.name }}</div>
+            </div>
         </div>
 
         <div class="flex flex-col space-y-2 my-2 ">
             <span class=" font-bold ">可獲得之福利:</span>
-            <span>1.</span>
-            <span>2.</span>
-            <span>3.</span>
+            <div v-for="(item, index) in getwelfare" :key="index" class="grid grid-cols-6 items-center gap-4 py-2 mx-3 border-b border-gray-300 last:border-0">
+                <div class="grid text-center ">{{ item.id }}</div>
+                <div class=" col-span-3">{{ item.name }}</div>
+            </div>
         </div>
 
 
-        <span><a class=" italic font my-2" :href="welfare.url">原文鏈接</a></span>
+        <span class=" font-bold">點此前往-> <a class=" italic font my-2 decoration-double underline text-blue-500" :href="welfare.url">原文鏈接</a></span>
         <span class="text-H3 my-2 leading-loose">原文 : {{ welfare.detail ? welfare.detail : "無法獲取原文" }}</span>
 
     </div>
@@ -49,8 +51,17 @@ onMounted(async () => {
 
 })
 
+const apply = ref([
+  { id: 1, name: '給羽毛錢錢' },
+  { id: 2, name: '稱讚羽毛' },
+  { id: 3, name: '讚嘆羽毛'},
+])
 
-
+const getwelfare = ref([
+    {id: 1, name:'羽毛讚賞的目光'},
+    {id: 2, name:'我想不到了'},
+    {id: 3, name:'我掰不出來了'},
+]) 
 
 
 </script>
