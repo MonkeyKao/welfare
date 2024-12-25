efc89213bc63e821c3f487c7b414f8dd41a42f68
Reina0513 <cherites0610@gmail.com>
2024-12-26 02:16:01 +0800

    feat: 更新設定頁面

--- frontend/src/views/User/SettingsPage.vue
+++ frontend/src/views/User/SettingsPage.vue
@@ -6,12 +6,20 @@
       <component :is="item.icon" :size="28" color="#4d4d4d" />
       <label class="text-H2 flex ml-3">{{ item.label }}</label>
     </div>
+    <div class="flex items-center justify-between">
+      <div class="flex items-center">
+        <PhAddressBook :size="28" color="#4d4d4d" />
+        <label class="text-H2 flex ml-3">自動篩選用戶資料</label>
+      </div>
+
+      <input type="checkbox" class="toggle toggle-success " />
+    </div>
   </div>
 </template>
 <script setup lang="ts">
 import HeaderBar from "@/components/HeaderBar.vue";
 import router from "@/router";
-import { PhLink, PhDetective } from "@phosphor-icons/vue";
+import { PhLink, PhDetective, PhAddressBook } from "@phosphor-icons/vue";
 const menuItems = [
   { path: "/user/link-account", label: "連結帳戶", icon: PhLink },
   { path: "/accessibility/home", label: "老人模式", icon: PhDetective },
